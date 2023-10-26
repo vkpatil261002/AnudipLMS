7dfb7f5edb926a631195f1b810f2d13feb3b91fa
Vaibhav K Patil <141112706+vkpatil261002@users.noreply.github.com>
2023-10-26 11:15:08 +0530

    Hello

---
+++ hello_World.java
@@ -0,0 +1,9 @@
+package LMS;
+public class hello_World 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(" Hello World! ");		//Use this statement for printing "Hello World" line
+	}
+	
+}
